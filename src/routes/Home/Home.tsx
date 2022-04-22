456f2b4e45b21f7cc996374a49a69f482a131814
nickisntavailable <kolda10.97@gmail.com>
2022-04-22 11:05:10 +0300

    removed imports

--- src/routes/Home/Home.tsx
+++ src/routes/Home/Home.tsx
@@ -1,11 +1,5 @@
 import NavItem from "../../components/NavItem/NavItem";
 
-import * as React from "react";
-import Radio from "@mui/material/Radio";
-import RadioGroup from "@mui/material/RadioGroup";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import FormControl from "@mui/material/FormControl";
-import FormLabel from "@mui/material/FormLabel";
 
 const navigationItems = [
   {
